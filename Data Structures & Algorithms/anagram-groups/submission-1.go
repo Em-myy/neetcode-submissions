36ec01b5346384265e6f6c549caf8101d15706ea
Famuyiwa Emmanuel <famuyiwaemmanuel565@gmail.com>
2026-07-26 23:03:31 +0100

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.go
@@ -0,0 +1,21 @@
+func groupAnagrams(strs []string) [][]string {
+	myMap := make(map[[26]int][]string)
+	var masterSlice [][]string 
+
+	for _, value := range strs {
+		var key [26]int
+		for _, val := range value {
+			if val >= 'a' && val <= 'z' {
+				key[val-'a']++
+			}
+		}
+
+		myMap[key] = append(myMap[key], value)
+	}
+
+	for _, value := range myMap {
+		masterSlice = append(masterSlice, value)
+	}
+
+	return masterSlice
+}
